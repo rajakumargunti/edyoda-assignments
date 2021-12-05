7870e921ceb6a0e065b0f66c0a9a6313d6cc8349
Rajakumar Gunti <rajakumargunti@gmail.com>
2021-12-05 14:03:30 +0530

    Implemented https://github.com/rkgunti/edyoda-assignments/issues/6#issue-1071390204

---
+++ assignment_3/numbers_functions_ex.py
@@ -0,0 +1,40 @@
+"""
+    # Game of "Functions"
+
+    Write a Python function to sum all the numbers in a list.
+    
+    Sample List :
+        (8, 2, 3, 0, 7)
+    
+    Expected Output :
+        20
+
+    Explanation:
+    Summation should like 8+2+3+0+7 = 20
+    
+    @Author: Rajakumar Gunti
+    @Date: Dec 5, 2021
+    @Links: https://classroom.edyoda.com/program-modules/DS291021/Python/1786/?type=assignment
+"""
+sample_list = (8, 2, 3, 0, 7)
+
+# Way 1
+def sum_of_list(list):
+    ' This is function will calucate the sum of all the numbers in a list '
+    sum = 0
+    for i in list:
+        sum += i
+    print(sum)
+
+sum_of_list(sample_list)
+
+# Way 2
+sum = 0
+def sum_of_list_2(list):
+    ' This is function will calucate the sum of all the numbers in a list '
+    for i in list:
+        global sum
+        sum += i
+
+sum_of_list_2(sample_list)
+print(sum)
